d2abdf6b926c4b20a84556c63c0e45c0c737f8a4
kenta-s <knt01222@gmail.com>
2017-05-27 16:16:07 +0900

    Implement NAND and OR

--- perceptron.py
+++ perceptron.py
@@ -9,3 +9,23 @@ def AND(x1, x2):
         return 0
     else:
         return 1
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    y = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(x*y)+b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    y = np.array([0.5, 0.5])
+    b = -0.2
+    tmp = np.sum(x*y)+b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
--- test_perceptron.py
+++ test_perceptron.py
@@ -7,5 +7,17 @@ class TestPerceptron(unittest.TestCase):
         self.assertEqual(0, perceptron.AND(0,1))
         self.assertEqual(0, perceptron.AND(0,0))
 
+    def test_NAND(self):
+        self.assertEqual(0, perceptron.NAND(1,1))
+        self.assertEqual(1, perceptron.NAND(0,1))
+        self.assertEqual(1, perceptron.NAND(1,0))
+        self.assertEqual(1, perceptron.NAND(0,0))
+
+    def test_OR(self):
+        self.assertEqual(1, perceptron.OR(1,1))
+        self.assertEqual(1, perceptron.OR(0,1))
+        self.assertEqual(1, perceptron.OR(1,0))
+        self.assertEqual(0, perceptron.OR(0,0))
+
 if __name__ == '__main__':
     unittest.main()
