68ba8caeab4282a8d1eee34c83f4a385d033adf2
kenta-s <knt01222@gmail.com>
2017-05-28 08:14:22 +0900

    Implement sigmoid function

--- test_neural_network.py
+++ test_neural_network.py
@@ -8,5 +8,9 @@ class TestNeuralNetwork(unittest.TestCase):
         # This leads to an error, I don't know why.
         # self.assertEqual(expected, neural_network.step_function(np.arange(-0.5, 0.5, 0.1)))
 
+    def test_sigmoid(self):
+        self.assertEqual(0.7310585786300049, neural_network.sigmoid(1.0))
+        self.assertEqual(0.88079707797788231, neural_network.sigmoid(2.0))
+
 if __name__ == '__main__':
     unittest.main()
