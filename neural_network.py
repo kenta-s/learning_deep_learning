f702f3f185e72f34fc94f9070f3e0902f76af9e1
kenta-s <knt01222@gmail.com>
2017-05-28 13:37:32 +0900

    Improve softmax

--- neural_network.py
+++ neural_network.py
@@ -34,7 +34,8 @@ def identity_function(x):
     return x
 
 def softmax(a):
-    exp_a = np.exp(a)
+    c = np.max(a)
+    exp_a = np.exp(a - c)
     sum_exp_a = np.sum(exp_a)
     y = exp_a / sum_exp_a
 
