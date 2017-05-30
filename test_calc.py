40e3b20e0fff4ae166f1854030f6653fb55cf3bc
kenta-s <knt01222@gmail.com>
2017-05-30 09:23:09 +0900

    some calculation methods

---
+++ test_calc.py
@@ -0,0 +1,23 @@
+import unittest
+import calc
+import numpy as np
+
+class TestCalc(unittest.TestCase):
+
+    def test_function_1(self):
+        # TODO: implement
+
+    def test_function_2(self):
+        # TODO: implement
+
+    def test_numerical_diff(self):
+        # TODO: implement
+
+    def test_numerical_gradient(self):
+        # TODO: implement
+
+    def test_gradient_descent(self):
+        # TODO: implement
+
+if __name__ == '__main__':
+    unittest.main()
